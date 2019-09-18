ed62aae8ed3875bd4416ef63819c1c6fee8e8adc
骆仕文 <shiwen@meri.co>
2019-09-19 02:42:45 +0800

    fix unused var

--- main.go
+++ main.go
@@ -3,8 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(sum9)
-	fmt.Println(sum10)
+	fmt.Println(sum8())
+	fmt.Println(sum9())
+	fmt.Println(sum10())
+}
+
+func sum8() int {
+	i1 := 1
+	i2 := 2
+	i3 := 3
+	i4 := 4
+	i5 := 5
+	i6 := 6
+	i7 := 7
+	i8 := 8
+
+	result := 0
+	result += i1
+	result += i2
+	result += i3
+	result += i4
+	result += i5
+	result += i6
+	result += i7
+	result += i8
+
+	return result
 }
 
 func sum9() int {
@@ -25,6 +49,7 @@ func sum9() int {
 	result += i4
 	result += i5
 	result += i6
+	result += i7
 	result += i8
 	result += i9
 
@@ -50,6 +75,7 @@ func sum10() int {
 	result += i4
 	result += i5
 	result += i6
+	result += i7
 	result += i8
 	result += i9
 	result += i10
