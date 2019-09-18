2a2622f0f01d9bd1bcfd21424989bf9deff1ca00
骆仕文 <shiwen@meri.co>
2019-09-19 02:27:11 +0800

    create sum9

--- main.go
+++ main.go
@@ -3,7 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println(sum9)
 	fmt.Println(sum10)
+}
+
+func sum9() int {
+	i1 := 1
+	i2 := 2
+	i3 := 3
+	i4 := 4
+	i5 := 5
+	i6 := 6
+	i7 := 7
+	i8 := 8
+	i9 := 9
+
+	result := 0
+	result += i1
+	result += i2
+	result += i3
+	result += i4
+	result += i5
+	result += i6
+	result += i8
+	result += i9
+
+	return result
 }
 
 func sum10() int {
